a970297a70d3488ea273d3e3acb09b1abc40ca72
jasp69 <h.munib23@gmail.com>
2023-02-02 22:04:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Budget from './components/Budget.js';
+import RemainingBudget from './components/Remaining.js';
+import ExpenseTotal from './components/ExpenseTotal.js';
+import ExpenseList from './components/ExpenseList.js';
+import AllocationForm from './components/AllocationForm.js';
+
+import { AppProvider } from './context/AppContext';
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+            <h1 className='mt-3'>Company's Budget Allocation</h1>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<Budget />
+					</div>
+					<div className='col-sm'>
+						<RemainingBudget />
+					</div>
+					<div className='col-sm'>
+						<ExpenseTotal />
+					</div>
+				</div>
+				<h3 className='mt-3'>Allocation</h3>
+				<div className='row '>
+					<div className='col-sm'>
+						<ExpenseList />
+					</div>
+				</div>
+				<h3 className='mt-3'>Change allocation</h3>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<AllocationForm/>
+					</div>
+				</div>
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
